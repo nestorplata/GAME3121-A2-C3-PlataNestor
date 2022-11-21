6449c2ac294c5bba50cc0d1894688625d63d9ca5
nestorplata <Nestor.PlataGomez@georgebrown.ca>
2022-11-21 15:11:11 -0500

    Challenge3

---
+++ GAME3121-A2-PlataNestor/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip boingSound;
+
+    public Vector3 deathposition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //changing this to public
+        playerRb = gameObject.GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if(gameOver)
+        {
+            playerRb.position = deathposition;
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.transform.position = gameObject.transform.position;
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+            gameObject.GetComponent<Rigidbody>().useGravity =false;
+            deathposition = gameObject.transform.position;
+            
+        }
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.transform.position = gameObject.transform.position;
+
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+
+        }
+
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            playerRb.AddForce(Vector3.up * floatForce*10);
+            playerAudio.PlayOneShot(boingSound, 1.0f);
+
+
+        }
+
+
+    }
+
+}
